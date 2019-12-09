22d53f2fe94234214408a11f92ef91a4bfeed2dc
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2019-12-09 09:10:12 -0800

    Final correction

--- src/tests/EventList.test.js
+++ src/tests/EventList.test.js
@@ -7,7 +7,7 @@ import Event from '../Event';
 describe('<app /> component', () => {
   test('render correct number of events', () => {
     const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-    EventListWrapper.setState({ events: [{}, {}, {}, {}] });
+
     expect(EventListWrapper.find(Event)).toHaveLength(4);
   });
 });
