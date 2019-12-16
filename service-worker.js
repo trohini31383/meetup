4cd3b4c3b47e65eb829ac1a3b160ad225e2188dd
trohini31383 <rohini.tomar@gmail.com>
2019-12-16 12:21:45 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.de2f0f3f7d6c9dda8cf3e25d549f7b93.js"
+  "/meetup/precache-manifest.6bb8db615ad7fa4628143df5aa91d655.js"
 );
 
 self.addEventListener('message', (event) => {
