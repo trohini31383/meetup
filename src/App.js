17c18c9a2efb798d00d21e3bf3d1e984b286e97f
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2020-01-08 09:53:57 -0800

    added charts

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import moment from 'moment';
 import { OfflineAlert } from './Alert';
 import { getEvents } from './api';
 import './App.css';
+import {
+
+  ScatterChart,
+
+  Scatter,
+
+  XAxis,
+
+  YAxis,
+
+  CartesianGrid,
+
+  Tooltip,
+
+  ResponsiveContainer
+
+} from 'recharts';
 
 
 
@@ -89,6 +107,50 @@ class App extends Component {
 
   };
 
+  countEventsOnADate = date => {
+
+    let count = 0;
+
+    for (let i = 0; i < this.state.events.length; i += 1) {
+
+      if (this.state.events[i].local_date === date) {
+
+        count += 1;
+
+      }
+
+    }
+
+    return count;
+
+  };
+
+  getData = () => {
+
+    const next7Days = []; // Create empty array for the next 7 days
+
+    const currentDate = moment(); // Today
+
+    // Loop 7 times for next 7 days
+
+    for (let i = 0; i < 7; i += 1) {
+
+      currentDate.add(1, 'days'); // Add 1 day to current date, currentDate changes
+
+      const dateString = currentDate.format('YYYY-MM-DD'); // Format the date
+
+      // Use the countEventsOnADate function to count #events on this date
+
+      const count = this.countEventsOnADate(dateString);
+
+      next7Days.push({ date: dateString, number: count }); // Add this date and number to the list
+
+    }
+
+    return next7Days;
+
+  };
+
 
 
   render() {
@@ -108,6 +170,32 @@ class App extends Component {
 
           lon={this.state.lon}
         />
+
+        <ResponsiveContainer height={400}>
+
+          <ScatterChart
+
+            margin={{
+
+              top: 20, right: 20, bottom: 20, left: 20,
+
+            }}
+
+          >
+
+            <CartesianGrid />
+
+            <XAxis type="category" dataKey="date" name="date" />
+
+            <YAxis type="number" dataKey="number" name="number of events" />
+
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+
+            <Scatter data={this.getData()} fill="#8884d8" />
+
+          </ScatterChart>
+
+        </ResponsiveContainer>
 
 
         <EventList events={this.state.events} />
