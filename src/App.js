41be159fac6f4dd211f9512d5d78de36554740d7
trohini31383 <53841712+trohini31383@users.noreply.github.com>
2020-01-08 10:13:43 -0800

    added charts

--- src/App.js
+++ src/App.js
@@ -175,6 +175,10 @@ class App extends Component {
 
           <ScatterChart
 
+            width={800}
+
+            height={400}
+
             margin={{
 
               top: 20, right: 20, bottom: 20, left: 20,
